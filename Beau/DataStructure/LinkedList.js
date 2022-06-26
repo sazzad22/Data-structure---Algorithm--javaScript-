e94b9c2c74fe3f86057118988a2226230a69e46d
Sazzad ur Rahman <22joylobo@gmail.com>
2022-06-26 21:14:05 +0600

    LinkedList init

---
+++ Beau/DataStructure/LinkedList.js
@@ -0,0 +1,11 @@
+//LinkedLists are linear data structures.
+//It contains nodes.Each node has it's value and a reference to it's next node.
+
+//3 types of linked lists - singly,doubly, circular linked list.
+
+//There are four basic operations of a linked list
+
+// addLast
+//removeLast
+//addFirst
+//removeFirst
