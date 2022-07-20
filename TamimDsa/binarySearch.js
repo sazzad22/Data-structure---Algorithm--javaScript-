3819968e19c665fecfcf4e7989ae1e75614eb818
Sazzad ur Rahman <22joylobo@gmail.com>
2022-07-20 08:43:32 +0600

    binary search tree

---
+++ TamimDsa/binarySearch.js
@@ -0,0 +1,25 @@
+//the funtion
+const binarySearch = (myArray, elementToSearch) => {
+    let left = 0;
+    let right = myArray.length - 1;
+    let mid;
+
+    //as long as left is less than right;
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2);
+        console.log(mid)
+        if (myArray[mid] === elementToSearch) {
+            return mid;
+        }
+        if (myArray[mid] < elementToSearch) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+        
+    }
+    return -1;
+}
+
+//calling the func
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 6))
